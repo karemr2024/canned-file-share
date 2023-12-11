761f5812eae815a5db61fef3d84c8f1ef8062cdc
Eric Xie <ericx@scc1.bu.edu>
2023-12-11 14:11:05 -0500

    Move+remove+helpers

--- NFT_chunks.cpp
+++ NFT_chunks.cpp
@@ -21,12 +21,31 @@ public:
   // Constructor
   TreeNode(std::string name, size_t size, std::string type,
            TreeNode *parentNode)
-      : fileName(name), fileSize(size), fileType(type), parent(parentNode) {
+    : fileName(name), fileSize(size), fileType(type), parent(parentNode) {
     timestampCreated = std::time(nullptr); // Set the current time as timestamp
   }
 
+  TreeNode(std::string name, std::string type,
+	   TreeNode* parentNode)
+    : fileName(name), fileType(type), parent(parentNode){
+    timestampCreated = std::time(nullptr);
+  }
+
+	   
+
   // Function to add a child to this node
   void addChild(TreeNode *child) { children.push_back(child); }
+
+  //Function to remove a child from this node, returns true if done successfully
+  bool removeChild(TreeNode* child){
+    for(int i=0;i<this->children.size();i++){
+      if(this->children[i]->fileName == child->fileName){
+	this->children.erase(this->children.begin()+i);
+	return true;
+      }
+    }
+    return false;
+  }
 };
 
 // A class for the tree structure itself
@@ -41,7 +60,6 @@ private:
 
     if (currentNode->fileName == name) {
       return currentNode;
-    }
 
     for (TreeNode *child : currentNode->children) {
       TreeNode *result = searchHelper(child, name);
@@ -79,58 +97,93 @@ public:
     return searchHelper(root, name);
   }
   void printTree() { printTreeHelper(root, 0); }
-  
+
+  void moveFile(std::string fileName,std::string destinationName){
+    TreeNode* file = this->searchByName(fileName);
+    TreeNode* destination = this->searchByName(destinationName);
+
+    //Check if file and destination folder exist
+    if(file == nullptr){
+      std::cout<<"File does not exist!" << std::endl;
+      return;
+    }
+    else if(destination == nullptr || destination->fileType != "folder"){
+      std::cout<<"Folder does not exist!" << std::endl;
+      return;
+    }
+
+    //Update parent/children relationships
+    file->parent->removeChild(file);
+    destination->addChild(file);
+    file->parent = destination;
+  }
+
+  //Store file in tree by creating new node and calling divide_chunks
+  bool storeFile(std::string filename,std::string type, std::string location){
+
+    TreeNode* folder = this->searchByName(location);
+
+    if(folder == nullptr || folder->filetype != "folder"){
+      std::cout<<"Invalid folder!" << std::endl;
+      return false;
+    }
+    
+    TreeNode* file = new TreeNode(filename,type,folder);
+    divide_chunks(filename, *file);
+
+    return true;
+  }
   // TODO: Implement the following functions
   /*
   
-  */
+   */
 };
 
 long unsigned int divide_chunks(string filename, TreeNode myNode) {
-	int chunkSize = 4096;
-	int fileSize = myNode.fileSize;
-	//char *file = filename;
+  int chunkSize = 4096;
+  int fileSize = myNode.fileSize;
+  //char *file = filename;
 
-	char *dest = (char *) malloc(chunkSize);
-	char ch;
+  char *dest = (char *) malloc(chunkSize);
+  char ch;
 
-	ifstream src_file;
-	const char *file_n = filename.c_str();
-	src_file.open(file_n);
-	FILE *pFile1;
-	FILE *pFile2;
-	pFile1 = fopen(file_n, "r");
+  ifstream src_file;
+  const char *file_n = filename.c_str();
+  src_file.open(file_n);
+  FILE *pFile1;
+  FILE *pFile2;
+  pFile1 = fopen(file_n, "r");
 
-	ifstream file;
-	file.open(filename, ifstream::binary);
-	ofstream ofile;
-	ofile.open(dest, ofstream::binary);
+  ifstream file;
+  file.open(filename, ifstream::binary);
+  ofstream ofile;
+  ofile.open(dest, ofstream::binary);
 
-	int numChunks = (fileSize + chunkSize - 1) /chunkSize;
+  int numChunks = (fileSize + chunkSize - 1) /chunkSize;
 
-	long unsigned int chunk_inds[numChunks];
-	
-	vector<char> buff (chunkSize,0);
-	streamsize s;
-	int count = 0;
-	while (!file.eof()) {
-		char *buffer = (char *) malloc(chunkSize);
-		file.read(buff.data(), buff.size());
-		s = file.gcount();
-		string str(buff.begin(), buff.end());
-		str.copy(buffer,4096,0);
-		chunk_inds[count] = (long unsigned int) buffer;
-		count++;
-//		ofiles.write(buff.data(), buff.size());
-		
-	}
-	myNode.chunk_inds = (long unsigned int) chunk_inds;
-	myNode.numChunks = numChunks;
-	return (long unsigned int) chunk_inds;
+  long unsigned int chunk_inds[numChunks];
+
+  vector<char> buff (chunkSize,0);
+  streamsize s;
+  int count = 0;
+  while (!file.eof()) {
+    char *buffer = (char *) malloc(chunkSize);
+    file.read(buff.data(), buff.size());
+    s = file.gcount();
+    string str(buff.begin(), buff.end());
+    str.copy(buffer,4096,0);
+    chunk_inds[count] = (long unsigned int) buffer;
+    count++;
+    //ofiles.write(buff.data(), buff.size());
+
+  }
+  myNode.chunk_inds = (long unsigned int) chunk_inds;
+  myNode.numChunks = numChunks;
+  return (long unsigned int) chunk_inds;
 }
 
 int main() {
-  // Example usage 
+  // Example usage
   FileTree myTree;
 
   // Creating the root node (typically a folder)
@@ -151,9 +204,11 @@ int main() {
   // Print the tree
   myTree.printTree();
 
-  long unsigned int test;
-  
-  test = divide_chunks("test.txt",*file1);
+  //long unsigned int test;
 
+  //  test = divide_chunks("test.txt",*file1);
+
+  myTree.moveFile("File1.txt","Flder2.md");
+  myTree.printTree();
   return 0;
 }
